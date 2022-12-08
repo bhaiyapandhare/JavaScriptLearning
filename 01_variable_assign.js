213e933ceda28fc0e7ac6fd65d8d10cd96427a06
bhaiyapandhare <bhaiyap78@gmail.com>
2022-12-08 17:55:57 +0530

    variableAssignment
    variableAssignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,9 @@
+var myFullName;
+myFullName= "Maruti Pandhare";
+console.log(myFullName)
+myFullName="Maruti Ambadas Pandhare";
+console.log(myFullName);
+var myLove="I Love Only JS";
+console.log(myLove);
+var myDetails="Maruti Pandhare 26 Sangola 413307 Male Unmarried ";
+console.log(myDetails);
