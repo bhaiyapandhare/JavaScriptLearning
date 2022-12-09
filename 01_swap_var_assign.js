f3c9f4b5e50b0214c7247b84ccb031584eafe7ff
bhaiyapandhare <bhaiyap78@gmail.com>
2022-12-09 09:41:33 +0530

    swapVarAssignment
    swapVarAssignment

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,33 @@
+console.log("==Before Swap==");
+var swetie="sweety";
+var cutie="cutie";
+console.log(swetie,cutie);
+var temp=swetie
+
+console.log("==After Swap==");
+var swetie="sweety";
+var cutie="cutie";
+console.log(swetie,cutie);
+var swetie=cutie
+var cutie=temp;
+console.log(swetie,cutie);
+
+console.log("===Number Swapping===");
+console.log("==Before Swap==");
+var num1=1000;
+var num2=2000;
+var num3=3000;
+
+console.log(num1,num2,num3);
+console.log("==After Swap==");
+var num1=1000;
+var num2=2000;
+var num3=3000;
+
+console.log(num1,num2,num3);
+var num4=num1;
+var num1=num2;
+var num2=num3;
+var num3=num4;
+console.log(num1,num2,num3);
+
