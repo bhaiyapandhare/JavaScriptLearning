56f6a63d65f9b2d88799297004f6348e10ceb94a
bhaiyapandhare <bhaiyap78@gmail.com>
2022-12-21 19:21:28 +0530

    02_function
    02_function

---
+++ 02_function.js
@@ -0,0 +1,17 @@
+// function show() {
+
+// }
+
+
+function swapValues(value1, value2){
+console.log("Before Swap", value1,value2);
+var temp= value1;
+value1= value2;
+value2= temp;
+console.log("After Swap", value1,value2 );
+
+}
+swapValues(100,200);
+swapValues(2,4);
+swapValues("Hitesh","Ritesh");
+swapValues("Komal","Yash");
