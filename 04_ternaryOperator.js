3d3c8a699228a33fb77afc8fe1b0a2ee60646151
bhaiyapandhare <bhaiyap78@gmail.com>
2022-12-21 19:19:34 +0530

    04_ternaryOperator
    04_ternaryOperator

---
+++ 04_ternaryOperator.js
@@ -0,0 +1,24 @@
+var sscMarks= 36;
+var resultPassOrFail= sscMarks>=35 ? "Pass": "fail";
+console.log(`Result is ${resultPassOrFail}`);
+
+var sscResult = function(){
+
+}
+
+var num1=33;
+var num2=35;
+var result= num1>num2? num1: num2;
+console.log(`Greater Num is ${result}`);
+
+var evenOrOdd= function(value){
+ value%2==0 ? "Even" : "Odd" ;
+ console.log(`Number is ${value}`);
+
+}
+evenOrOdd(29);
+
+// var evenOrOdd: (value: 25) => void
+// var evenOrOdd: (value: 44) => void
+// var evenOrOdd: (value: 0) => void
+// var evenOrOdd: (value: 101) => void
