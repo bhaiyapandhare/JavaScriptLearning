785bf1233cb41ca3bc789637fdacf68016165762
bhaiyapandhare <bhaiyap78@gmail.com>
2022-12-21 18:54:26 +0530

    05_switchAssignment
    05_switchAssignment

---
+++ 05_switchAssignments.js
@@ -0,0 +1,323 @@
+var monthNumber =1;
+switch (monthNumber) {
+    case 1:
+    console.log(`January`);
+    console.log(`This is January frist month of the year`); 
+        break;
+        case 2:        console.log(`Februwary`);
+        console.log(`This is Februwary second month of the year`);
+        break;
+        case 3:
+    console.log(`March`);
+    console.log(`This is March third month of the year`);
+    break;
+    case 4:
+    console.log(`April`);
+    console.log(`This is April fourth month of the year`);
+    break;
+    case 5:
+    console.log(`May`);
+    console.log(`This is May fifth month of the year`);
+    break;
+    case 6:
+    console.log(`June`);
+    console.log(`This is June sixth month of the year`);
+    break;
+    case 7:
+    console.log(`July`);
+    console.log(`This is July seventh month of the year`);
+    break;
+    case 8:
+    console.log(`Auguest`);
+    console.log(`This is Auguest eightth month of the year`);
+    break;
+    case 9:
+    console.log(`September`);
+    console.log(`This is September ninth month of the year`);
+    break;
+    case 10:
+    console.log(`October`);
+    console.log(`This is May tenth month of the year`);
+    break;
+    case 11:
+    console.log(`November`);
+    console.log(`This is November eleventh month of the year`);
+    break;
+    case 12:
+    console.log(`December`);
+    console.log(`This is December twelweth month of the year`);
+    break;
+    default:
+        console.log(`Not a valid value Provide value between 1 To 12`);
+        break;
+}
+
+var monthNumber =12;
+switch (monthNumber) {
+    case 1:
+    console.log(`January`);
+    console.log(`This is January frist month of the year`); 
+        break;
+        case 2:        console.log(`Februwary`);
+        console.log(`This is Februwary second month of the year`);
+        break;
+        case 3:
+    console.log(`March`);
+    console.log(`This is March third month of the year`);
+    break;
+    case 4:
+    console.log(`April`);
+    console.log(`This is April fourth month of the year`);
+    break;
+    case 5:
+    console.log(`May`);
+    console.log(`This is May fifth month of the year`);
+    break;
+    case 6:
+    console.log(`June`);
+    console.log(`This is June sixth month of the year`);
+    break;
+    case 7:
+    console.log(`July`);
+    console.log(`This is July seventh month of the year`);
+    break;
+    case 8:
+    console.log(`Auguest`);
+    console.log(`This is Auguest eightth month of the year`);
+    break;
+    case 9:
+    console.log(`September`);
+    console.log(`This is September ninth month of the year`);
+    break;
+    case 10:
+    console.log(`October`);
+    console.log(`This is May tenth month of the year`);
+    break;
+    case 11:
+    console.log(`November`);
+    console.log(`This is November eleventh month of the year`);
+    break;
+    case 12:
+    console.log(`December`);
+    console.log(`This is December twelweth month of the year`);
+    break;
+    default:
+        console.log(`Not a valid value Provide value between 1 To 12`);
+        break;
+}
+
+var monthNumber = 3;
+switch (monthNumber) {
+    case 1:
+    console.log(`January`);
+    console.log(`This is January frist month of the year`); 
+        break;
+        case 2:        console.log(`Februwary`);
+        console.log(`This is Februwary second month of the year`);
+        break;
+        case 3:
+    console.log(`March`);
+    console.log(`This is March third month of the year`);
+    break;
+    case 4:
+    console.log(`April`);
+    console.log(`This is April fourth month of the year`);
+    break;
+    case 5:
+    console.log(`May`);
+    console.log(`This is May fifth month of the year`);
+    break;
+    case 6:
+    console.log(`June`);
+    console.log(`This is June sixth month of the year`);
+    break;
+    case 7:
+    console.log(`July`);
+    console.log(`This is July seventh month of the year`);
+    break;
+    case 8:
+    console.log(`Auguest`);
+    console.log(`This is Auguest eightth month of the year`);
+    break;
+    case 9:
+    console.log(`September`);
+    console.log(`This is September ninth month of the year`);
+    break;
+    case 10:
+    console.log(`October`);
+    console.log(`This is May tenth month of the year`);
+    break;
+    case 11:
+    console.log(`November`);
+    console.log(`This is November eleventh month of the year`);
+    break;
+    case 12:
+    console.log(`December`);
+    console.log(`This is December twelweth month of the year`);
+    break;
+    default:
+        console.log(`Not a valid value Provide value between 1 To 12`);
+        break;
+}
+
+var monthNumber =2;
+switch (monthNumber) {
+    case 1:
+    console.log(`January`);
+    console.log(`This is January frist month of the year`); 
+        break;
+        case 2:        console.log(`Februwary`);
+        console.log(`This is Februwary second month of the year`);
+        break;
+        case 3:
+    console.log(`March`);
+    console.log(`This is March third month of the year`);
+    break;
+    case 4:
+    console.log(`April`);
+    console.log(`This is April fourth month of the year`);
+    break;
+    case 5:
+    console.log(`May`);
+    console.log(`This is May fifth month of the year`);
+    break;
+    case 6:
+    console.log(`June`);
+    console.log(`This is June sixth month of the year`);
+    break;
+    case 7:
+    console.log(`July`);
+    console.log(`This is July seventh month of the year`);
+    break;
+    case 8:
+    console.log(`Auguest`);
+    console.log(`This is Auguest eightth month of the year`);
+    break;
+    case 9:
+    console.log(`September`);
+    console.log(`This is September ninth month of the year`);
+    break;
+    case 10:
+    console.log(`October`);
+    console.log(`This is May tenth month of the year`);
+    break;
+    case 11:
+    console.log(`November`);
+    console.log(`This is November eleventh month of the year`);
+    break;
+    case 12:
+    console.log(`December`);
+    console.log(`This is December twelweth month of the year`);
+    break;
+    default:
+        console.log(`Not a valid value Provide value between 1 To 12`);
+        break;
+}
+
+var monthNumber =9;
+switch (monthNumber) {
+    case 1:
+    console.log(`January`);
+    console.log(`This is January frist month of the year`); 
+        break;
+        case 2:        console.log(`Februwary`);
+        console.log(`This is Februwary second month of the year`);
+        break;
+        case 3:
+    console.log(`March`);
+    console.log(`This is March third month of the year`);
+    break;
+    case 4:
+    console.log(`April`);
+    console.log(`This is April fourth month of the year`);
+    break;
+    case 5:
+    console.log(`May`);
+    console.log(`This is May fifth month of the year`);
+    break;
+    case 6:
+    console.log(`June`);
+    console.log(`This is June sixth month of the year`);
+    break;
+    case 7:
+    console.log(`July`);
+    console.log(`This is July seventh month of the year`);
+    break;
+    case 8:
+    console.log(`Auguest`);
+    console.log(`This is Auguest eightth month of the year`);
+    break;
+    case 9:
+    console.log(`September`);
+    console.log(`This is September ninth month of the year`);
+    break;
+    case 10:
+    console.log(`October`);
+    console.log(`This is May tenth month of the year`);
+    break;
+    case 11:
+    console.log(`November`);
+    console.log(`This is November eleventh month of the year`);
+    break;
+    case 12:
+    console.log(`December`);
+    console.log(`This is December twelweth month of the year`);
+    break;
+    default:
+        console.log(`Not a valid value Provide value between 1 To 12`);
+        break;
+}
+
+var monthNumber =14;
+switch (monthNumber) {
+    case 1:
+    console.log(`January`);
+    console.log(`This is January frist month of the year`); 
+        break;
+        case 2:        console.log(`Februwary`);
+        console.log(`This is Februwary second month of the year`);
+        break;
+        case 3:
+    console.log(`March`);
+    console.log(`This is March third month of the year`);
+    break;
+    case 4:
+    console.log(`April`);
+    console.log(`This is April fourth month of the year`);
+    break;
+    case 5:
+    console.log(`May`);
+    console.log(`This is May fifth month of the year`);
+    break;
+    case 6:
+    console.log(`June`);
+    console.log(`This is June sixth month of the year`);
+    break;
+    case 7:
+    console.log(`July`);
+    console.log(`This is July seventh month of the year`);
+    break;
+    case 8:
+    console.log(`Auguest`);
+    console.log(`This is Auguest eightth month of the year`);
+    break;
+    case 9:
+    console.log(`September`);
+    console.log(`This is September ninth month of the year`);
+    break;
+    case 10:
+    console.log(`October`);
+    console.log(`This is May tenth month of the year`);
+    break;
+    case 11:
+    console.log(`November`);
+    console.log(`This is November eleventh month of the year`);
+    break;
+    case 12:
+    console.log(`December`);
+    console.log(`This is December twelweth month of the year`);
+    break;
+    default:
+        console.log(`Not a valid value Provide value between 1 To 12`);
+        break;
+}
