7532a7b160e19f05dc9e58e7b8e050ed7a95215d
bhaiyapandhare <bhaiyap78@gmail.com>
2023-01-02 19:10:09 +0530

    07_objectAssignmentUpdate
    07_objectAssignmentUpdate

--- 07_objectAssignment.js
+++ 07_objectAssignment.js
@@ -51,50 +51,49 @@ delete professor.certificates.hackerRank ;
 console.log(professor);
 
 
-console.log("Accesing a Nested object property ");
+// console.log("Accesing a Nested object property ");
 
-console.log(professor.degrees.degree);
-
-
+// console.log(professor.degrees.degree);
 
 
 
-// console.log("========Assignment O2========");
 
-// const sbiBank:{
-//     bankName:"State Bank of India";
-//     location:"Deccan Gynkhana";
-//     accountNo: 351024561122;
-//     ifsc: "SBIN000111";
-//     interestRate:"8%";
-// }
-// console.log(sbiBank);
 
-// const axisBank :{
-// bankName:"Axis Bank",
-// location:"Fergusson College Road",
-// accountNo:915810002002,
-// ifsc:"UTIB0000037",
-// interestRate:"8.5%",
-// }
-// console.log(axisBank);
+console.log("========Assignment O2========");
 
-// const hdfcBank:{
-// bankName: "HDFC Bank",
-// location:"Bund Garden Rd",
-// accountNo:800023781133,
-// ifsc:"HDFC0004629",
-// interestRate:"8.7%",
-// }
-// console.log(hdfcBank);
+const sbiBank={
+    bankName:"State Bank of India",
+    location:"Deccan Gynkhana",
+    accountNo: 351024561122,
+    ifsc: "SBIN000111",
+    interestRate:"8%",
+}
+console.log(sbiBank);
 
-// const yesBank:{
-// bankName4:"Yes Bank",
-// location:"Shivaji Nagar ",
-// accountNo:112234500567,
-// ifsc:YESB0000008
-// interestRate:"8.6%",
-// }
+const axisBank ={
+bankName:"Axis Bank",
+location:"Fergusson College Road",
+accountNo:915810002002,
+ifsc:"UTIB0000037",
+interestRate:"8.5%",
+}
+console.log(axisBank);
 
-// console.log(yesBank);
+const hdfcBank={
+bankName: "HDFC Bank",
+location:"Bund Garden Rd",
+accountNo:800023781133,
+ifsc:"HDFC0004629",
+interestRate:"8.7%",
+}
+console.log(hdfcBank);
 
+const yesBank={
+bankName4:"Yes Bank",
+location:"Shivaji Nagar ",
+accountNo:112234500567,
+ifsc:"YESB0000008",
+interestRate:"8.6%",
+}
+
+console.log(yesBank);
