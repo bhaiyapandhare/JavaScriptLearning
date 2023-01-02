ac7afe6ea92ad2c639ef572d455f48f11f0014d7
bhaiyapandhare <bhaiyap78@gmail.com>
2023-01-02 19:12:06 +0530

    updateHello
    updateHello

--- hello.js
+++ hello.js
@@ -1,1 +1,39 @@
 console.log("Hello Maruti");
+console.log("========Assignment O2========");
+
+const sbiBank: {
+    bankName: "State Bank of India";
+    location: "Deccan Gynkhana";
+    accountNo: 351024561122;
+    ifsc: "SBIN0001110";
+    interestRate: "8%";
+}
+console.log(sbiBank);
+
+const axisBank :{
+bankName:"Axis Bank",
+location:"Fergusson College Road",
+accountNo:915810002002,
+ifsc:"UTIB0000037",
+interestRate:"8.5%",
+}
+console.log(axisBank);
+
+const hdfcBank:{
+bankName: "HDFC Bank",
+location:"Bund Garden Rd",
+accountNo:800023781133,
+ifsc:"HDFC0004629",
+interestRate:"8.7%",
+}
+console.log(hdfcBank);
+
+const yesBank:{
+bankName:"Yes Bank",
+location:"Shivaji Nagar ",
+accountNo:112234500567,
+ifsc:YESB0000008
+interestRate:"8.6%",
+}
+
+console.log(yesBank);
