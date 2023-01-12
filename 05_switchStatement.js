8c0f8c0b5817a1335a1464d71d711c0a17b211e9
bhaiyapandhare <bhaiyap78@gmail.com>
2023-01-12 06:57:33 +0530

    05_switchStatement.js
    05_switchStatement.js

---
+++ 05_switchStatement.js
@@ -0,0 +1,89 @@
+var dayNumber= 2;
+switch (dayNumber) {
+    case 1:
+        console.log(`Today is Monday `);
+        console.log(` It is Start Of Week`);
+        break;
+case 2:
+    console.log(`Today is Tuesday`);
+    break;
+    case 3:
+        console.log(`Today is Wednesday`);
+        break;
+case 4:
+    console.log(`Today is Thursday`);
+    break;
+    case 5:
+        console.log(`Today is Friday`);
+        break;
+        case 6:
+            console.log(`Today is Saturday`);
+            break;
+            case 7:
+                console.log(`Today is Sunday`);
+                console.log(`Sunday is Holiday`);
+        break;
+
+    default:
+        break;
+}
+
+var dayNumber= 1;
+switch (dayNumber) {
+    case 1:
+        console.log(`Today is Monday `);
+        console.log(` It is Start Of Week`);
+        break;
+case 2:
+    console.log(`Today is Tuesday`);
+    break;
+    case 3:
+        console.log(`Today is Wednesday`);
+        break;
+case 4:
+    console.log(`Today is Thursday`);
+    break;
+    case 5:
+        console.log(`Today is Friday`);
+        break;
+        case 6:
+            console.log(`Today is Saturday`);
+            break;
+            case 7:
+                console.log(`Today is Sunday`);
+                console.log(`Sunday is Holiday`);
+        break;
+
+    default:
+        break;
+}
+
+var dayNumber= 7;
+switch (dayNumber) {
+    case 1:
+        console.log(`Today is Monday `);
+        console.log(` It is Start Of Week`);
+        break;
+case 2:
+    console.log(`Today is Tuesday`);
+    break;
+    case 3:
+        console.log(`Today is Wednesday`);
+        break;
+case 4:
+    console.log(`Today is Thursday`);
+    break;
+    case 5:
+        console.log(`Today is Friday`);
+        break;
+        case 6:
+            console.log(`Today is Saturday`);
+            break;
+            case 7:
+                console.log(`Today is Sunday`);
+                console.log(`Sunday is Holiday`);
+        break;
+
+    default:
+        break;
+}
