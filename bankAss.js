4aa9b5b8d937b7d4c4d9d01ec1556ed04c2cdbed
bhaiyapandhare <bhaiyap78@gmail.com>
2023-01-12 07:10:29 +0530

    bankAss.js
    bankAss.js

---
+++ bankAss.js
@@ -0,0 +1,42 @@
+console.log("========Assignment O2========");
+
+const sbiBank:{
+    bankName:"State Bank of India";
+    location:"Deccan Gynkhana";
+    accountNo: 351024561122;
+    ifsc: "SBIN000111";
+    interestRate:"8%";
+}
+console.log(sbiBank);
+
+const bankName :{
+bankName:"Axis Bank",
+location:"Fergusson College Road",
+accountNo:915810002002,
+ifsc:"UTIB0000037",
+interestRate:"8.5%",
+}
+console.log(axisBank);
+
+const hdfcBank:{
+bankName: "HDFC Bank",
+location:"Bund Garden Rd",
+accountNo:800023781133,
+ifsc:"HDFC0004629",
+interestRate:"8.7%",
+}
+console.log(hdfcBank);
+
+const yesBank:{
+bankName4:"Yes Bank",
+location:"Shivaji Nagar ",
+accountNo:112234500567,
+ifsc:YESB0000008
+interestRate:"8.6%",
+}
+
+console.log(yesBank);
+
+"State Bank of India","Deccan Gynkhana","SBIN000111","8%"
+
+"Axis Bank","Fergusson College Road","UTIB0000037","8.5%",
